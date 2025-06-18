93ebcb760e97e73a18dbb11c441ffcf912ded555
GensengG <168531024+GensengG@users.noreply.github.com>
2025-06-19 00:29:11 +0300

    Update Logout.jsx

--- src/ClientComponents/Logout.jsx
+++ src/ClientComponents/Logout.jsx
@@ -36,7 +36,7 @@ export const Logout = () => {
                 <Admin />
             ));
         } else {
-            // alert(result.error);
+            alert(result.error);
         } 
     }          
 
